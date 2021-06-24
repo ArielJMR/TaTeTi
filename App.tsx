f2d2b38159b18f7659a4ebd9adfa86e28c7d751d
ArielJMR <juanarielmr35@gmail.com>
2021-06-24 16:49:16 -0300

    feature/project-start

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import TaTeTi from './components/TaTeTi';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(): JSX.Element {
+    return (
+        <View style={styles.body}>
+            <TaTeTi />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    body: {
+        margin: 0,
+        fontFamily: 'Helvetica, sans-serif',
+    }
 });
